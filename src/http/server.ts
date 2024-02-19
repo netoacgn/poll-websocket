2f8efe46c2a726e3ef3ad3a55bd0e7b5da79038a
Antonio <acgomesn@gmail.com>
2024-02-19 18:09:38 -0300

    install zod to validation + route to create a poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,25 @@
 import fastify from "fastify";
+import { z } from 'zod';
+import { PrismaClient } from '@prisma/client';
 
-const app = fastify()
+const app = fastify();
 
-app.get('/hello', (req, res) => {
-    
+const prisma = new PrismaClient();
+
+app.post('/polls', async (req, reply) => {
+    const createPollBody = z.object({
+        title: z.string(),
+    })
+
+    const { title } = createPollBody.parse(req.body);
+
+    const poll = await prisma.poll.create({
+        data: {
+            title: title,
+        }
+    })
+
+    return reply.status(201).send({ pollId: poll.id })
 })
 
 app.listen({ port: 3333 }).then(() => {
