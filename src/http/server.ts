2c0aa0fb0abb88f54988def5731cf456ad001b1b
Antonio <acgomesn@gmail.com>
2024-02-20 17:43:42 -0300

    fix poll creating route + table to poll options

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,26 +1,11 @@
 import fastify from "fastify";
 import { z } from 'zod';
-import { PrismaClient } from '@prisma/client';
+import { prisma } from "../lib/prisma";
+import { createPoll } from "./routes/create-polls";
 
 const app = fastify();
 
-const prisma = new PrismaClient();
-
-app.post('/polls', async (req, reply) => {
-    const createPollBody = z.object({
-        title: z.string(),
-    })
-
-    const { title } = createPollBody.parse(req.body);
-
-    const poll = await prisma.poll.create({
-        data: {
-            title: title,
-        }
-    })
-
-    return reply.status(201).send({ pollId: poll.id })
-})
+app.register(createPoll);
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running!');
