6bcecbdd4fcb8c01cd43711169f4baf0be17c99a
Antonio <acgomesn@gmail.com>
2024-02-20 17:50:23 -0300

    route to get poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,10 +2,12 @@ import fastify from "fastify";
 import { z } from 'zod';
 import { prisma } from "../lib/prisma";
 import { createPoll } from "./routes/create-polls";
+import { getPoll } from "./routes/get-poll";
 
 const app = fastify();
 
 app.register(createPoll);
+app.register(getPoll);
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running!');
