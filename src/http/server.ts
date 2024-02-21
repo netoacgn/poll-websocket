d9a83b6978e097cdfe2f1aefa7ce5800bab2e57a
Antonio <acgomesn@gmail.com>
2024-02-20 23:19:46 -0300

    create route to vote on poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,11 +3,19 @@ import { z } from 'zod';
 import { prisma } from "../lib/prisma";
 import { createPoll } from "./routes/create-polls";
 import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
+import cookie from "@fastify/cookie";
 
 const app = fastify();
 
+app.register(cookie, {
+    secret: "polls-app-ws",
+    hook: 'onRequest',
+});
+
 app.register(createPoll);
 app.register(getPoll);
+app.register(voteOnPoll);
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running!');
