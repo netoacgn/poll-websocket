34e89a43db85fb089b031c77b0747a03a4a7dab3
Antonio <acgomesn@gmail.com>
2024-02-22 22:02:43 -0300

    create count in redis to the poll option

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 import { prisma } from "../../lib/prisma";
 import { FastifyInstance } from "fastify";
 import { randomUUID } from 'node:crypto';
+import { redis } from "../../lib/redis";
 
 export async function voteOnPoll(app: FastifyInstance) {
     app.post('/polls/:pollId/votes', async (req, reply) => {
@@ -35,6 +36,8 @@ export async function voteOnPoll(app: FastifyInstance) {
                     }
                 })
 
+                await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+
             } else if (userPreviousVoteOnPoll) {
                 return reply.status(400).send({ message: 'You already voted on this poll.' })
             }
@@ -60,6 +63,7 @@ export async function voteOnPoll(app: FastifyInstance) {
             }
         })
 
+        await redis.zincrby(pollId, 1, pollOptionId)
 
         return reply.status(201).send({ sessionId });
     })
