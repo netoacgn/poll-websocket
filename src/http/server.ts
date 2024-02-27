0ba185424afaa965faf9163688956b639018f112
Antonio <acgomesn@gmail.com>
2024-02-26 22:51:02 -0300

    using pattern pub/sub

--- src/http/server.ts
+++ src/http/server.ts
@@ -6,6 +6,7 @@ import { getPoll } from "./routes/get-poll";
 import { voteOnPoll } from "./routes/vote-on-poll";
 import cookie from "@fastify/cookie";
 import websocket from "@fastify/websocket";
+import { pollResults } from "./ws/poll-results";
 
 const app = fastify();
 
@@ -19,6 +20,7 @@ app.register(websocket);
 app.register(createPoll);
 app.register(getPoll);
 app.register(voteOnPoll);
+app.register(pollResults);
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running!');
